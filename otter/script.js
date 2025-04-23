fc46087c0bcb2ee102111846ff4394c6ea33faa9
nathan <50345854+superpotato9@users.noreply.github.com>
2025-04-22 19:52:08 -0700

    added full preload in js

--- otter/script.js
+++ otter/script.js
@@ -4,18 +4,31 @@ function getRandomInt(max) {
 function formatNumber(num) {
     return String(num).padStart(3, '0');
 }
+var cache = document.createElement("CACHE");
+cache.style = "position:absolute;z-index:-1000;opacity:0;";
+document.body.appendChild(cache);
+function preloadImage(url) {
+    var img = new Image();
+    img.src = url;
+    img.style = "position:absolute";
+    cache.appendChild(img);
+}
+
 
 const Dvd_logo = document.querySelector(".ball");
 
 Dvd_logo.style.backgroundImage = "url('photos/0" + formatNumber(getRandomInt(186)) + ".jpg')"
 
 let iterationCount = 0;
-
+let background = "photos/0" + formatNumber(getRandomInt(186)) + ".jpg";
 Dvd_logo.addEventListener("animationiteration", () => {
 
     iterationCount++;
     console.log(`Animation iteration count: ${iterationCount}`);
-    Dvd_logo.style.backgroundImage = "url('photos/0" + formatNumber(getRandomInt(186)) + ".jpg')"
+    Dvd_logo.style.backgroundImage = "url(" + background +")";
+    console.log(background);
+    background = "photos/0" + formatNumber(getRandomInt(186)) + ".jpg";
+    preloadImage(background);
 
 });
 
